d717fca50df194e8beb92afa0398bbaaaed2f700
Jacob Lodenius <jacoblodenius@gmail.com>
2020-05-16 14:00:30 +0200

    feature scaling

--- data_preprocessing/data_preprocessing.py
+++ data_preprocessing/data_preprocessing.py
@@ -26,6 +26,10 @@ y = le.fit_transform(y)
 
 # Split dataset to training and test set
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
-# Feature scaling (standardisation / normalisation)
+# Feature scaling (standardisation)
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train[:, 3:] = sc.fit_transform(x_train[:, 3:])
+x_test[:, 3:] = sc.transform(x_test[:, 3:])
